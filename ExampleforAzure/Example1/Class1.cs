4cdb8bc6d21cde3244210e58b39e319e896eb8bb
Niveditha201997 <113034709+Niveditha201997@users.noreply.github.com>
2022-10-20 10:21:50 +0530

    Add files via upload

---
+++ ExampleforAzure/Example1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Example1
+{
+    public class Class1
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ ExampleforAzure/ExampleforAzure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleforAzure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
